7edfc0201cd44e7d757f3f5fb45b12515a2a7a0e
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-11 19:37:34 +0100

    Created Post model.

--- network/models.py
+++ network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, to_field='username', on_delete=models.CASCADE, related_name='User')
+    text = models.CharField(max_length=400, null=False)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, blank=True)
+
