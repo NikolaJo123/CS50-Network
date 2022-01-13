e608d24c17f34129168add89110ba737abdaa6cb
NikolaJo123 <nikola_ATE@hotmail.com>
2022-01-13 17:57:03 +0100

    Created API route for show posts.

--- network/urls.py
+++ network/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+
+    # API Routes
+    path("show_post/", views.show_post, name="show_post"),
 ]
